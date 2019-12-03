6d91ea809f1a3b8ec492d54e0df136420270acf6
ditd799 <mibtres@yahoo.com>
2019-12-03 12:14:43 -0600

    inicio

---
+++ action.js
@@ -0,0 +1,29 @@
+function login() {
+	// body...
+    window.location = "./page/index.html";
+	
+} 
+function loginantony() {
+	// body...
+	window.location = "./page/inder.html";
+}
+function loginvictor() {
+	// body...
+	window.location = "./page/index.html";
+}
+function loginmichi() {
+	// body...
+	window.location = "./page/indes.html";
+}
+
+ function menu() {
+ 	// body...
+ 	document.body.innerHTML="<nav>"+
+ 	"<div>index</div> <div>muro</div> <div>mensajes</div>" + "</nav>"
+ }
+
+
+ window.addEventListener('load',function(){
+ 	$("section").fadeIn(2000);
+ })
+
